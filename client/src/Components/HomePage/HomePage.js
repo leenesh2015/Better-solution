29476b805dc94b800a6711f8ccbc0acc37f5d038
Leenesh Sawner <leenesh2015@gmail.com>
2021-03-03 10:53:35 +0530

    Data Flitering Application

---
+++ client/src/Components/HomePage/HomePage.js
@@ -0,0 +1,21 @@
+import logo from "../../10x.jpeg";
+import "./HomePage.css";
+import React from "react";
+
+
+function HomePage() {
+	return (
+		<div >
+				<img className="All" src={logo} alt="here"></img>				
+				
+							<div className="Alpa">10x Team 02</div>
+
+							<button className="bbutton">Enter</button>
+						</div>
+
+						
+					
+				
+	);
+}
+export default HomePage;
